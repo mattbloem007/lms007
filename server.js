231bf5a9d0cbe07aad0c521e918ce8426c71c435
mikeb008 <mikeb008@gmail.com>
2018-07-19 13:20:52 +0200

    Changed database connection to display error

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ connection.connect(function(err){
 if(!err) {
     console.log("Database is connected ... nn");
 } else {
-    console.log("Error connecting database ... nn");
+    console.log("Error connecting database ... nn", err);
 }
 });
 
