773b24fa350d6d36a493ff8c3fe3acf38c7c2143
mikeb008 <mikeb008@gmail.com>
2018-07-31 09:26:57 +0200

    removed port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path')
 
 
 const app = express();
-const port = 22322; //process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client/build')));
